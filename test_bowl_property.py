61c6d577d4f22e7d786856cc22ee1d2617c19502
Gavin Leake <gavin@land.tech>
2022-07-29 11:52:18 +0100

    bonus: constrain frames to roll <=10

--- test_bowl_property.py
+++ test_bowl_property.py
@@ -4,7 +4,14 @@ from hypothesis import example, given, strategies as st
 throw = st.integers(min_value=0, max_value=10)
 
 
-@given(frames=st.lists(st.tuples(throw, st.one_of(st.none(), throw)), max_size=10))
+@given(
+    frames=st.lists(
+        st.tuples(throw, st.one_of(st.none(), throw)).filter(
+            lambda f: f[0] + (f[1] or 0) <= 10
+        ),
+        max_size=10,
+    )
+)
 @example(frames=[(10, None) for _ in range(10)])
 def test_fuzz_score(frames):
     result = bowl.score(frames=frames)
